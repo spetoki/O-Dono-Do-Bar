9f1cc9be8274b1c1292a5385380a37c707212b87
Irineu Marcos <spetoki@gmail.com>
2025-08-04 18:21:31 +0000

    crie o arquivo env com as chaves

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  projectId: "adegga-cb479",
-  appId: "1:235447536219:web:658ec72e01bf70d10c8d7e",
-  storageBucket: "adegga-cb479.firebasestorage.app",
-  apiKey: "AIzaSyCCw1xG4XvfXea1cfILShTABurIdpw4n2o",
-  authDomain: "adegga-cb479.firebaseapp.com",
-  messagingSenderId: "235447536219",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 // Initialize Firebase
